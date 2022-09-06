dd02823420eea7a402a720f00245865b54222711
장일규 <jang19.dev@gmail.com>
2022-09-06 09:21:24 +0900

    Add: weatherTitleLabel 추가 및 레이아웃 설정

--- WeatherInfoApp/WeatherInfoApp/Controllers/WeatherController.swift
+++ WeatherInfoApp/WeatherInfoApp/Controllers/WeatherController.swift
@@ -8,11 +8,42 @@
 import UIKit
 
 class WeatherController: UIViewController {
+    
+    let weatherTitleLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "도시별 현재 날씨"
+        lb.font = WeatherFont.Bold.of(size: 30)
+        lb.textColor = WeatherColor.darkGray.color
+        return lb
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setup()
     }
+    
+    private func setup() {
+        view.backgroundColor = .white
+        addViews()
+        setConstraints()
+    }
+    
+    private func addViews() {
+        view.addSubview(weatherTitleLabel)
+    }
+    
+    private func setConstraints() {
+        weatherTitleLabelConstraints()
+    }
+    
+    private func weatherTitleLabelConstraints() {
+        weatherTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        weatherTitleLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+        weatherTitleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        weatherTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        weatherTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    }
+
 
 
 }
