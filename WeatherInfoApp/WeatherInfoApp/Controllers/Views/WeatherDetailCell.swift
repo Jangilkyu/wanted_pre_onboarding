8f6875169ef7b585f7fa77245bbe35e1470af21a
장일규 <jang19.dev@gmail.com>
2022-09-06 09:34:24 +0900

    Add: View폴더 생성 후 WeatherDetailCell 생성

---
+++ WeatherInfoApp/WeatherInfoApp/Controllers/Views/WeatherDetailCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherDetailCell.swift
+//  WeatherInfoApp
+//
+//  Created by jangilkyu on 2022/09/06.
+//
+
+import UIKit
+
+class WeatherDetailCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+      return nil
+    }
+}
